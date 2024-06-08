c1f4b64211819bf9050222e9ee5eab4f5f042451
Danimaba <d.mateo@sapalomera.cat>
2024-06-08 18:42:25 +0200

    Primer commit desde el portatil, actualización de repositorio

---
+++ operadoresLogicos.js
@@ -0,0 +1,24 @@
+/* 📌 Operadores Lógicos 
+	&& 	And
+	||	OR
+	! 	NOT
+*/
+
+// Permitir acceso a un evento si una persona es mayor de edad
+//let nombre = 'Dani';
+// let edad = 24   ;
+
+// let permitirAcceso = edad >= 18;
+// console.log('Acceso permitido al concierto: ' + permitirAcceso)
+// let permitirAcceso2 = edad >= 18 ? 'El usuario tiene acceso al concierto.' : 'El usuario NO tiene acceso al concierto';
+// console.log(permitirAcceso2);
+/* let cliente = {
+    edad: 15,
+    nombre: 'Dani',
+    entrada: false
+};
+// let permitirAcceso3 = cliente.edad >= 18 && cliente.entrada ? 'El usuario tiene acceso al concierto.' : 'El usuario NO tiene acceso al concierto';
+// console.log(permitirAcceso3);
+let permiso = true;
+let permitirAcceso4 = (cliente.edad >= 18 && cliente.entrada) || permiso ? 'El usuario tiene acceso al concierto.' : 'El usuario NO tiene acceso al concierto';
+console.log(permitirAcceso4); */
