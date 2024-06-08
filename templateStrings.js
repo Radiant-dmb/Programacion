84ea0199555b48a1bb43eb740a6049f6cb105b1e
Radiant-dmb <danki_87@live.com>
2024-06-08 20:04:45 +0200

    Saneado de carpetas

--- templateStrings.js
+++
@@ -1,6 +0,0 @@
-const nombre = 'Carlos Arturo';
-const edad = 24;
-const pais = 'España';
-
-console.log('La persona ' + nombre + 'tiene ' + edad + 'años.')
-console.log(`La persona ${nombre} tiene ${edad} años.`)
